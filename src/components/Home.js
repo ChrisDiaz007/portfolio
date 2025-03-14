1c82804b298c614f3ca1b0f89f58f57951391140
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-03-14 13:37:34 +0900

    Added portfolio img & changed proect text to white for easy read

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,7 +39,7 @@ const Home = () => {
       </div>
       <div className="avatar">
         <div className="card">
-          <img src="avatar.jpg" alt="" />
+          <img src="demoday.jpg" alt="" />
           <div className="info">
             <div>Developer</div>
             <div>Hispanic</div>
